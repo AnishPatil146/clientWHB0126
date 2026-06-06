1bc9f96b914bf530e82662fba19da29918862131
Anish Patil <anishpatil146@gmail.com>
2026-06-06 14:05:54 +0530

    refactor: simplify Vercel configuration and update CORS policy to allow all origins

--- server.js
+++ server.js
@@ -28,7 +28,9 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: (origin, callback) => {
+      callback(null, true);
+    },
     credentials: true
   }
 });
@@ -81,7 +83,7 @@ if (gmailReady) {
 }
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: true,
   credentials: true
 }));
 app.use(express.json());
